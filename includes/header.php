53ea7203ab2f9d79aaf71af0f0338315e0f5dc6b
Moussa WANE CAMARA <154606621+Moussa-data@users.noreply.github.comls>
2026-03-08 12:57:32 +0100

    Move username above navigation

--- includes/header.php
+++ includes/header.php
@@ -1,37 +1,31 @@
 <?php
-// Démarrage de la session si elle n'est pas déjà active
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
+$userName = !empty($_SESSION['user']['pseudo']) ? $_SESSION['user']['pseudo'] : null;
 ?>
 
-<header>
-  <div class="logo">EcoRide</div>
-
-<nav>
-  <a href="index.php">Accueil</a>
-  <a href="covoiturages_db.php">Covoiturages</a>
-  <a href="contact.php">Contact</a>
-
- <?php if (!empty($_SESSION['user'])): ?>
-  <div class="user-box">
-    <span class="user-label">Connecté :</span>
-    <span class="user-name"><?= htmlspecialchars($_SESSION['user']['pseudo']); ?></span>
-  </div>
-
-  <a href="create_ride.php">Proposer un trajet</a>
-  <a href="profile.php">Mon profil</a>
-  <a href="logout.php">Déconnexion</a>
-<?php else: ?>
-  <a href="login.php">Connexion</a>
-  <a href="register.php">Inscription</a>
-<?php endif; ?>
-    <a href="create_ride.php">Proposer un trajet</a>
-    <a href="profile.php">Mon profil</a>
-    <a href="logout.php">Déconnexion</a>
-  <?php else: ?>
-    <a href="login.php">Connexion</a>
-    <a href="register.php">Inscription</a>
+<header class="site-header">
+  <?php if ($userName): ?>
+    <div class="user-banner">
+      <span class="user-label">Bonjour</span>
+      <span class="user-name"><?= htmlspecialchars($userName) ?></span>
+    </div>
   <?php endif; ?>
-</nav>
+
+  <nav class="main-nav">
+    <a href="index.php">Accueil</a>
+    <a href="covoiturages_db.php">Covoiturages</a>
+    <a href="contact.php">Contact</a>
+
+    <?php if ($userName): ?>
+      <a href="create_ride.php">Proposer un trajet</a>
+      <a href="profile.php">Mon profil</a>
+      <a href="logout.php">Déconnexion</a>
+    <?php else: ?>
+      <a href="login.php">Connexion</a>
+      <a href="register.php">Inscription</a>
+    <?php endif; ?>
+  </nav>
 </header>
