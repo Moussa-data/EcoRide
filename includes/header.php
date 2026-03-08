234e2246760aaed4bbfbc6253f357c2de8850d15
Moussa WANE CAMARA <154606621+Moussa-data@users.noreply.github.comls>
2026-03-08 12:39:30 +0100

    Remove test_db file

--- includes/header.php
+++ includes/header.php
@@ -13,10 +13,19 @@ if (session_status() === PHP_SESSION_NONE) {
   <a href="covoiturages_db.php">Covoiturages</a>
   <a href="contact.php">Contact</a>
 
-  <?php if (!empty($_SESSION['user'])): ?>
-    <span style="font-weight:600; color:white;">
-      Bonjour <?= htmlspecialchars($_SESSION['user']['pseudo']); ?>
-    </span>
+ <?php if (!empty($_SESSION['user'])): ?>
+  <div class="user-box">
+    <span class="user-label">Connecté :</span>
+    <span class="user-name"><?= htmlspecialchars($_SESSION['user']['pseudo']); ?></span>
+  </div>
+
+  <a href="create_ride.php">Proposer un trajet</a>
+  <a href="profile.php">Mon profil</a>
+  <a href="logout.php">Déconnexion</a>
+<?php else: ?>
+  <a href="login.php">Connexion</a>
+  <a href="register.php">Inscription</a>
+<?php endif; ?>
     <a href="create_ride.php">Proposer un trajet</a>
     <a href="profile.php">Mon profil</a>
     <a href="logout.php">Déconnexion</a>
--- test_db.php
+++
@@ -1,3 +0,0 @@
-<?php
-require __DIR__ . "/includes/db_connect.php";
-echo "✅ Connexion MySQL OK";
